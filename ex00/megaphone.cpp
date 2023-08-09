851c8b77b8ea4f9e5887640650d5e737fb367247
Rida Rhnizar <rrhnizar@c1r2p9.1337.ma>
2023-08-09 19:04:40 +0100

    finish ex00

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,15 +1,38 @@
-
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rrhnizar <rrhnizar@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/09 18:45:28 by rrhnizar          #+#    #+#             */
+/*   Updated: 2023/08/09 19:04:17 by rrhnizar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include <iostream>
+#include <ctype.h>
 
 int	main(int argc, char **argv)
 {
-	if (argc == 2)
+	int	i;
+	int	j;
+
+	i = 1;
+	if (argc >= 2)
 	{
-		std::cout<<argv[1];
+		while (argv[i])
+		{
+			j = 0;
+			while(argv[i][j])
+				std::cout<<(char)toupper(argv[i][j++]);
+			if (argv[i + 1])
+				std::cout<< " ";
+			i++;
+		}
 	}
 	else
 		std::cout<<"* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-	std::cout<<"\n";
+	std::cout << std::endl;
 	return 0;
 }
